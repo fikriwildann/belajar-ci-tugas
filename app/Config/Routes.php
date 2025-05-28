dafc6f113d5e6a8c9e0bc63f5e58aeb24702c9bc
fikriwildann <fikri.wildan.ariyananda@gmail.com>
2025-05-28 13:19:59 +0700

    first commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -17,6 +17,7 @@ $routes->group('produk', ['filter' => 'auth'], function ($routes) {
     $routes->post('', 'ProdukController::create');
     $routes->post('edit/(:any)', 'ProdukController::edit/$1');
     $routes->get('delete/(:any)', 'ProdukController::delete/$1');
+    $routes->get('download', 'ProdukController::download');
 });
 
 $routes->group('produk_kategori', ['filter' => 'auth'], function ($routes) { 
@@ -24,6 +25,14 @@ $routes->group('produk_kategori', ['filter' => 'auth'], function ($routes) {
     $routes->post('', 'ProdukKategoriController::create');
     $routes->post('edit/(:any)', 'ProdukKategoriController::edit/$1');
     $routes->get('delete/(:any)', 'ProdukKategoriController::delete/$1');
+});
+
+$routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
+    $routes->get('', 'TransaksiController::index');
+    $routes->post('', 'TransaksiController::cart_add');
+    $routes->post('edit', 'TransaksiController::cart_edit');
+    $routes->get('delete/(:any)', 'TransaksiController::cart_delete/$1');
+    $routes->get('clear', 'TransaksiController::cart_clear');
 });
 
 $routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
